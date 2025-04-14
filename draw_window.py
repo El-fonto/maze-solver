508028f187c294c3e55eaa5d6e6c378b325bae61
El-fonto <laotrafuente880@gmail.com>
2025-04-14 16:04:09 -0600

    init, redraw and wait for close methods. Main needs refactoring

--- draw_window.py
+++ draw_window.py
@@ -3,10 +3,31 @@ from tkinter import Tk, BOTH, Canvas
 
 class Window:
     def __init__(self, width: int, height: int) -> None:
-        self.width = width
-        self.height = height
-        self.root = Tk()
-        self.root.title()
-        self.canvas = Canvas()
-        self.canvas.pack()
-        self.running = False
+        self.__root = Tk()
+        self.__root.title("Maze Solver")
+        self.__canvas = Canvas(self.__root, bg="white", height=height, width=width)
+        self.__canvas.pack(fill=BOTH, expand=1)
+        self.__running = False
+        self.__root.protocol("WM_DELETE_WINDOW", self.close())
+
+    def redraw(self):
+        self.__root.update_idletasks()
+        self.__root.update()
+
+    def wait_for_close(self):
+        self.__running = True
+        while self.__running is True:
+            self.redraw()
+        print("window closed...")
+
+    def close(self):
+        self.__running = False
+
+
+def main():
+    win = Window(800, 600)
+    win.wait_for_close()
+
+
+if __name__ == "__main__":
+    main()
